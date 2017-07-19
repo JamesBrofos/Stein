95ddd6dbf686fda1d8a7b14a3518647ca653220d
JamesBrofos <jamesbrofos@gmail.com>
2017-07-19 08:33:25 -0400

    more pythonic distributed sampler

--- stein/samplers/distributed_stein_sampler.py
+++ stein/samplers/distributed_stein_sampler.py
@@ -2,7 +2,7 @@ import numpy as np
 import tensorflow as tf
 from mpi4py import MPI
 from .abstract_stein_sampler import AbstractSteinSampler
-from ..utilities import convert_dictionary_to_array
+from ..utilities import convert_dictionary_to_array, convert_array_to_dictionary
 
 
 class DistributedSteinSampler(AbstractSteinSampler):
@@ -21,11 +21,13 @@ class DistributedSteinSampler(AbstractSteinSampler):
                 n_particles, log_p, gd, theta
             )
         else:
+            # TensorFlow setup on the worker processes.
             self.model_vars = tf.get_collection(
                 tf.GraphKeys.TRAINABLE_VARIABLES, "model"
             )
             self.grad_log_p = tf.gradients(log_p, self.model_vars)
             self.sess = tf.Session()
+            self.tf_variables = {v.name: v for v in self.model_vars}
 
         # Initialize a dictionary to store the gradient with respect to each
         # constituent parameter of the particle.
@@ -34,60 +36,81 @@ class DistributedSteinSampler(AbstractSteinSampler):
         ]))
         self.n_workers = self.comm.size - 1
         self.particles_per_worker = n_particles // self.n_workers
-        if n_particles % self.n_workers != 0:
+        if n_particles % self.n_workers != 0 and self.comm.rank == 0:
             raise ValueError(
                 "The number of particles must be divisible by the number of "
                 "worker processes."
             )
 
+    def master_process(self):
+        """This is the master process. The master process is responsible for
+        coordinating the gradient computations and combining them to update the
+        optimal perturbation direction.
+        """
+        ppw = self.particles_per_worker
+        work = [
+            (i, i + ppw) for i in range(0, self.n_particles - ppw + 1, ppw)
+        ]
+        for i in range(self.n_workers):
+            param_dict = {
+                v.name: self.theta[v][work[i][0]:work[i][1]]
+                for v in self.model_vars
+            }
+            self.comm.send(param_dict, dest=i+1)
+
+        # Reassemble the matrix of gradients.
+        G = np.empty((self.n_particles, self.n_params))
+        for i in range(self.n_workers):
+            # Initialize status for the receive and a buffer into which the
+            # computed gradient will be loaded.
+            s = MPI.Status()
+            # Receive the gradient and place it into the corresponding rows of
+            # the larger gradient matrix across all processes.
+            g = self.comm.recv(source=MPI.ANY_SOURCE, status=s)
+            w = work[s.source-1]
+            G[w[0]:w[1]] = g
+
+        # Apply the optimal perturbation direction.
+        self.update_particles(G)
+
+    def worker_process(self, batch_feed):
+        """This is the worker process. It is responsible for computing the
+        gradient of the posterior log-likelihood with respect to the model
+        parameters.
+
+        Parameters:
+            batch_feed (dict): A dictionary that maps TensorFlow placeholders to
+                provided values. For instance, this might be mappings of feature
+                and target placeholders to batch values. Notice that this feed
+                dictionary will be internally augmented to include the current
+                feed values for the model parameters for each particle.
+        """
+        # Extract the transmitted dictionary into a dictionary with the same
+        # value but with TensorFlow variable keys.
+        data = self.comm.recv(source=0)
+        theta_feed = {self.tf_variables[name]: data[name] for name in data}
+        # Initialize an empty array for the gradients for each particle assigned
+        # to this worker process.
+        for i in range(self.particles_per_worker):
+            batch_feed.update({v: theta_feed[v][i] for v in theta_feed})
+            g = self.sess.run(self.grad_log_p, batch_feed)
+            if i == 0:
+                F = [np.expand_dims(h, 0) for h in g]
+            else:
+                F = [
+                    np.vstack((F[i], np.expand_dims(h, 0)))
+                    for i, h in enumerate(g)
+                ]
+            G, _ = convert_dictionary_to_array({
+                v: g for v, g in zip(self.model_vars, F)
+            })
+
+        # Send the gradient back to the master node.
+        self.comm.send(G, dest=0)
+
     def train_on_batch(self, batch_feed):
         """Implementation of abstract base class method."""
         if self.comm.rank == 0:
-            # This is the master process. The master process is responsible for
-            # coordinating the gradient computations and combining them to
-            # update the optimal perturbation direction.
-            work = []
-            work_indices = 0
-            for i in range(self.n_workers):
-                work.append((work_indices, work_indices + self.particles_per_worker))
-                param_dict = {
-                    v.name: self.theta[v][work[-1][0]:work[-1][1]]
-                    for v in self.model_vars
-                }
-                work_indices += self.particles_per_worker
-                self.comm.send(param_dict, dest=i+1)
-
-            # Reassemble the matrix of gradients.
-            G = np.empty((self.n_particles, self.n_params))
-            for i in range(self.n_workers):
-                s = MPI.Status()
-                g = np.empty((self.particles_per_worker, self.n_params))
-                self.comm.Recv([g, MPI.FLOAT], source=MPI.ANY_SOURCE, status=s)
-                w = work[s.source-1]
-                G[w[0]:w[1]] = g
-
-            # Apply the optimal perturbation direction.
-            self.update_particles(G)
+            self.master_process()
         else:
-            # This is the worker process. It is responsible for computing the
-            # gradient of the posterior log-likelihood with respect to the model
-            # parameters.
-            data = self.comm.recv(source=0)
-            theta_feed = {}
-            for i, v in enumerate(data):
-                var = next((x for x in self.model_vars if x.name == v), None)
-                theta_feed[var] = data[v]
-            # Save space by clearing out the transmitted dictionary.
-            del data
-            # Initialize an empty array for the gradients for each particle
-            # assigned to this worker process.
-            G = np.empty((self.particles_per_worker, self.n_params))
-            for i in range(self.particles_per_worker):
-                batch_feed.update({v: theta_feed[v][i] for v in theta_feed})
-                g = self.sess.run(self.grad_log_p, batch_feed)
-                G[i] = convert_dictionary_to_array({
-                    v: np.expand_dims(g, 0) for v, g in zip(self.model_vars, g)
-                })[0].ravel()
-
-            # Send the gradient back to the master node.
-            self.comm.Send([G, MPI.FLOAT], dest=0)
+            self.worker_process(batch_feed)
